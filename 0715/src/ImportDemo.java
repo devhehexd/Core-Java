497dd17f69619644ca3cee72ad94cd672e8fc9b6
cruz <rlqja72201@gmail.com>
2024-07-15 17:47:03 +0900

    7월 15일 자바 수업

--- 0715/src/ImportDemo.java
+++ 0715/src/ImportDemo.java
@@ -1,12 +1,18 @@
-import kr.seoul.sesac.util.libs.*;
+//import kr.seoul.sesac.util.libs.*;
+import com.example.libs.Car;
 
-public class ImportDemo {
+public class ImportDemo  extends Car{
     public static void main(String[] args) {
     	
-    	Finance f = new Finance();
-    	HumanResource hr = new HumanResource();
-    	System.out.println(f.money);
-    	System.out.println(hr.salary);
+//    	Finance f = new Finance();
+//    	HumanResource hr = new HumanResource();
+//    	System.out.println(f.money);
+//    	System.out.println(hr.salary);
+    	Car sonata = new Car();
+    	System.out.println(sonata.name);
+    	ImportDemo id = new ImportDemo();
+        System.out.println(id.price);
+    	
 
     }
 }
